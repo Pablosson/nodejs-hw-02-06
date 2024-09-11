73b786cea382e75f34f47407b1e2d17223c46d7a
PabloG <pawelgarbula@gmail.com>
2024-09-11 15:11:40 +0200

    fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -37,12 +37,12 @@ router.get("/:contactId", async (req, res, next) => {
     const { contactId } = req.params;
     const getContact = await getContactById(contactId);
     if (!getContact) {
-      res.json({
+      return res.status(404).json({
         message: `Contact with id "${contactId}" not found`,
         status: 404,
       });
     }
-    res.json({
+    res.status(200).json({
       status: 200,
       data: { getContact },
     });
@@ -53,9 +53,9 @@ router.get("/:contactId", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const contactBody = contactSchema.validate(req.body);
-    if (contactBody.error?.message) {
-      res.status(400).json({ message: contactBody.error.message });
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.message });
     }
     const newContact = await addContact(req.body);
     res.status(201).json({ data: { newContact } });
@@ -69,12 +69,12 @@ router.delete("/:contactId", async (req, res, next) => {
     const { contactId } = req.params;
     const deleteContact = await removeContact(contactId);
     if (!deleteContact) {
-      res.json({
+      return res.status(404).json({
         message: "Contact not found",
         status: 404,
       });
     }
-    res.json({
+    res.status(200).json({
       message: "Contact deleted",
       status: 200,
     });
@@ -85,14 +85,14 @@ router.delete("/:contactId", async (req, res, next) => {
 
 router.put("/:contactId", async (req, res, next) => {
   try {
-    const contactBody = contactSchema.validate(req.body);
-    if (contactBody.error?.message) {
-      res.status(400).json({ message: contactBody.error.message });
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.message });
     }
     const { contactId } = req.params;
     const updateOldContact = await updateContact(contactId, req.body);
     if (!updateOldContact) {
-      res.status(404).json({ message: "contact not found" });
+      return res.status(404).json({ message: "Contact not found" });
     }
     res.status(200).json({ data: { updateOldContact } });
   } catch (error) {
@@ -102,13 +102,17 @@ router.put("/:contactId", async (req, res, next) => {
 
 router.patch("/:contactId/favorite", async (req, res, next) => {
   try {
-    const { contactId } = req.params;
     const { favorite } = req.body;
-    const setFavorite = await updateFavorite(contactId, favorite);
-    if (favorite === undefined || null) {
-      res.json({ status: 400, message: "missing field favorite" });
+    if (typeof favorite !== "boolean") {
+      return res.status(400).json({ message: "missing field favorite" });
     }
-    res.json({ status: 200, data: { setFavorite } });
+
+    const { contactId } = req.params;
+    const setFavorite = await updateFavorite(contactId, favorite);
+    if (!setFavorite) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+    res.status(200).json({ data: { setFavorite } });
   } catch (error) {
     next(error);
   }
